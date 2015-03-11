9c3ad62d38330c69d525eac666bb8ad102d119bb
sheenvempeny <sheen_vempeny@hotmail.com>
2015-03-11 18:14:22 +0400

    frames

--- PicFlow++/DetailViewController.swift
+++ PicFlow++/DetailViewController.swift
@@ -10,6 +10,7 @@ import Foundation
 class DetailViewController: UIViewController,RAReorderableLayoutDelegate, RAReorderableLayoutDataSource {
     
     
+    @IBOutlet weak var imageScrollView: UIScrollView!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var picCollectionView: UICollectionView!
 
@@ -42,6 +43,7 @@ class DetailViewController: UIViewController,RAReorderableLayoutDelegate, RAReor
         self.picCollectionView.delegate = self
         self.picCollectionView.dataSource = self
         (self.picCollectionView.collectionViewLayout as RAReorderableLayout).scrollDirection = .Horizontal
+        self.setupScrollView();
     }
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
@@ -53,8 +55,12 @@ class DetailViewController: UIViewController,RAReorderableLayoutDelegate, RAReor
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewWillAppear(animated:Bool) {
+        super.viewWillAppear(animated);
     
-    
+        // Setup the scrollview scales on viewWillAppear
+        self.setupScales()
+    }
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
     {
@@ -73,15 +79,15 @@ class DetailViewController: UIViewController,RAReorderableLayoutDelegate, RAReor
     }
     
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
-        return CGSizeMake(130.0, 170.0)
+        return CGSizeMake(120.0, 100.0)
     }
     
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
-        return UIEdgeInsetsMake(0, 20.0, 0, 20.0)
+        return UIEdgeInsetsMake(0, 0.0, 0, 0.0)
     }
     
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat {
-        return 20.0
+        return 1.0
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
@@ -97,7 +103,7 @@ class DetailViewController: UIViewController,RAReorderableLayoutDelegate, RAReor
     }
     
     func scrollTrigerEdgeInsetsInCollectionView(collectionView: UICollectionView) -> UIEdgeInsets {
-        return UIEdgeInsetsMake(0, 50, 0, 50)
+        return UIEdgeInsetsMake(0, 5, 0, 5)
     }
     
     func scrollSpeedValueInCollectionView(collectionView: UICollectionView) -> CGFloat {
@@ -106,8 +112,104 @@ class DetailViewController: UIViewController,RAReorderableLayoutDelegate, RAReor
     
     func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath)
     {
+        
         var frame:Frame = self.frames()![indexPath.item]
         imageView.image = frame.image
+        self.imageView.frame = CGRectMake(0, 0, frame.image!.size.width, frame.image!.size.height)
+        // Tell the scroll view the size of the contents
+        self.imageScrollView.contentSize = frame.image!.size;
+       self.setupScales()
+        
+    }
+    
+    func setupScrollView() -> Void
+    {
+        
+        // Sets the scrollview delegate as self
+        self.imageScrollView.delegate = self;
+        
+        // Add doubleTap recognizer to the scrollView
+        //  UITapGestureRecognizer *doubleTapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(scrollViewDoubleTapped:)];
+        var doubleTapRecognizer:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "scrollViewDoubleTapped:")
+        doubleTapRecognizer.numberOfTapsRequired = 2;
+        doubleTapRecognizer.numberOfTouchesRequired = 1;
+        self.imageScrollView.addGestureRecognizer(doubleTapRecognizer);
+        
+        // Add two finger recognizer to the scrollView
+        var twoFingerTapRecognizer:UITapGestureRecognizer  = UITapGestureRecognizer(target: self, action: "scrollViewTwoFingerTapped:")
+        twoFingerTapRecognizer.numberOfTapsRequired = 1;
+        twoFingerTapRecognizer.numberOfTouchesRequired = 2;
+        self.imageScrollView.addGestureRecognizer(twoFingerTapRecognizer);
+        
+    }
+
+    func setupScales() {
+    // Set up the minimum & maximum zoom scales
+        var scrollViewFrame:CGRect = self.imageScrollView.frame;
+        var scaleWidth:CGFloat = scrollViewFrame.size.width / self.imageScrollView.contentSize.width;
+        var scaleHeight:CGFloat = scrollViewFrame.size.height / self.imageScrollView.contentSize.height;
+        var minScale:CGFloat  = min(scaleWidth, scaleHeight);
+        self.imageScrollView.minimumZoomScale = minScale;
+        self.imageScrollView.maximumZoomScale = 1.0;
+        self.imageScrollView.zoomScale = minScale;
+        self.centerScrollViewContents()
+    }
+    
+    func centerScrollViewContents() {
+        // This method centers the scroll view contents also used on did zoom
+        var boundsSize:CGSize = self.imageScrollView.bounds.size;
+        var contentsFrame:CGRect  = self.imageView.frame;
+        
+        if (contentsFrame.size.width < boundsSize.width) {
+            contentsFrame.origin.x = (boundsSize.width - contentsFrame.size.width) / 2.0;
+        } else {
+            contentsFrame.origin.x = 0.0;
+        }
+        
+        if (contentsFrame.size.height < boundsSize.height) {
+            contentsFrame.origin.y = (boundsSize.height - contentsFrame.size.height) / 2.0;
+        } else {
+            contentsFrame.origin.y = 0.0;
+        }
+        
+        self.imageView.frame = contentsFrame;
+    }
+    func viewForZoomingInScrollView(scrollView:UIScrollView) -> UIView{
+        // Return the view that we want to zoom
+        return self.imageView;
+    }
+    
+    func scrollViewDidZoom(scrollView:UIScrollView) {
+    // The scroll view has zoomed, so we need to re-center the contents
+        self.centerScrollViewContents()
+    }
+    
+    func scrollViewDoubleTapped(recognizer:UITapGestureRecognizer) {
+    // Get the location within the image view where we tapped
+       var pointInView:CGPoint = recognizer.locationInView(self.imageView)
+    
+    // Get a zoom scale that's zoomed in slightly, capped at the maximum zoom scale specified by the scroll view
+        var newZoomScale:CGFloat = self.imageScrollView.zoomScale * 1.5;
+        newZoomScale = min(newZoomScale, self.imageScrollView.maximumZoomScale);
+    
+    // Figure out the rect we want to zoom to, then zoom to it
+        var scrollViewSize:CGSize = self.imageScrollView.bounds.size;
+    
+        var w:CGFloat = scrollViewSize.width / newZoomScale;
+        var h:CGFloat = scrollViewSize.height / newZoomScale;
+        var x:CGFloat = pointInView.x - (w / 2.0);
+        var y:CGFloat = pointInView.y - (h / 2.0);
+    
+        var rectToZoomTo:CGRect = CGRectMake(x, y, w, h);
+        self.imageScrollView.zoomToRect(rectToZoomTo, animated: true)
+    }
+    
+    func scrollViewTwoFingerTapped(recognizer:UITapGestureRecognizer) {
+        // Zoom out slightly, capping at the minimum zoom scale specified by the scroll view
+        var newZoomScale:CGFloat = self.imageScrollView.zoomScale / 1.5;
+        newZoomScale = max(newZoomScale, self.imageScrollView.minimumZoomScale);
+        self.imageScrollView.setZoomScale(newZoomScale, animated: true)
+        
     }
 
 }
@@ -139,7 +241,15 @@ class FrameCell: UICollectionViewCell
     }
     
       private func configure() {
-        self.imageView = UIImageView(frame: self.bounds)
+        
+        let backImageView:UIImageView = UIImageView(frame: self.bounds)
+        [self.backgroundView?.removeFromSuperview()]
+        self.backgroundView = backImageView
+        backImageView.image = UIImage(named: "film.png")
+       
+        
+        var imageRect:CGRect = CGRectMake(5.0, 10.0, CGRectGetWidth(self.bounds) - 10.0, CGRectGetHeight(self.bounds) - 20.0)
+        self.imageView = UIImageView(frame: imageRect)
         self.imageView.autoresizingMask = .FlexibleWidth | .FlexibleHeight
         self.contentView.addSubview(imageView)
     }
