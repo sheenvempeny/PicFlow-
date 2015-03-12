74810b142db14e6d829cd779611a176d65a1b31b
sheenvempeny <sheen_vempeny@hotmail.com>
2015-03-12 18:11:03 +0400

    image zoom fixed

--- PicFlow++/DetailViewController.swift
+++ PicFlow++/DetailViewController.swift
@@ -9,10 +9,9 @@
 import Foundation
 class DetailViewController: UIViewController,RAReorderableLayoutDelegate, RAReorderableLayoutDataSource {
     
-    
-    @IBOutlet weak var imageScrollView: UIScrollView!
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var picCollectionView: UICollectionView!
+    var visibleRect:CGRect = CGRectZero
+    @IBOutlet weak var imageScrollView: YRImageZoomingView!
+   @IBOutlet weak var picCollectionView: UICollectionView!
 
     func getProject() -> Project?
     {
@@ -43,7 +42,7 @@ class DetailViewController: UIViewController,RAReorderableLayoutDelegate, RAReor
         self.picCollectionView.delegate = self
         self.picCollectionView.dataSource = self
         (self.picCollectionView.collectionViewLayout as RAReorderableLayout).scrollDirection = .Horizontal
-        self.setupScrollView();
+       
     }
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
@@ -59,7 +58,7 @@ class DetailViewController: UIViewController,RAReorderableLayoutDelegate, RAReor
         super.viewWillAppear(animated);
     
         // Setup the scrollview scales on viewWillAppear
-        self.setupScales()
+        //self.setupScales()
     }
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
@@ -114,104 +113,12 @@ class DetailViewController: UIViewController,RAReorderableLayoutDelegate, RAReor
     {
         
         var frame:Frame = self.frames()![indexPath.item]
-        imageView.image = frame.image
-        self.imageView.frame = CGRectMake(0, 0, frame.image!.size.width, frame.image!.size.height)
-        // Tell the scroll view the size of the contents
-        self.imageScrollView.contentSize = frame.image!.size;
-       self.setupScales()
+        imageScrollView.image = frame.image
         
-    }
-    
-    func setupScrollView() -> Void
-    {
         
-        // Sets the scrollview delegate as self
-        self.imageScrollView.delegate = self;
-        
-        // Add doubleTap recognizer to the scrollView
-        //  UITapGestureRecognizer *doubleTapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(scrollViewDoubleTapped:)];
-        var doubleTapRecognizer:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "scrollViewDoubleTapped:")
-        doubleTapRecognizer.numberOfTapsRequired = 2;
-        doubleTapRecognizer.numberOfTouchesRequired = 1;
-        self.imageScrollView.addGestureRecognizer(doubleTapRecognizer);
-        
-        // Add two finger recognizer to the scrollView
-        var twoFingerTapRecognizer:UITapGestureRecognizer  = UITapGestureRecognizer(target: self, action: "scrollViewTwoFingerTapped:")
-        twoFingerTapRecognizer.numberOfTapsRequired = 1;
-        twoFingerTapRecognizer.numberOfTouchesRequired = 2;
-        self.imageScrollView.addGestureRecognizer(twoFingerTapRecognizer);
-        
-    }
+    }//Dictionary<String,UIView>
 
-    func setupScales() {
-    // Set up the minimum & maximum zoom scales
-        var scrollViewFrame:CGRect = self.imageScrollView.frame;
-        var scaleWidth:CGFloat = scrollViewFrame.size.width / self.imageScrollView.contentSize.width;
-        var scaleHeight:CGFloat = scrollViewFrame.size.height / self.imageScrollView.contentSize.height;
-        var minScale:CGFloat  = min(scaleWidth, scaleHeight);
-        self.imageScrollView.minimumZoomScale = minScale;
-        self.imageScrollView.maximumZoomScale = 1.0;
-        self.imageScrollView.zoomScale = minScale;
-        self.centerScrollViewContents()
-    }
-    
-    func centerScrollViewContents() {
-        // This method centers the scroll view contents also used on did zoom
-        var boundsSize:CGSize = self.imageScrollView.bounds.size;
-        var contentsFrame:CGRect  = self.imageView.frame;
-        
-        if (contentsFrame.size.width < boundsSize.width) {
-            contentsFrame.origin.x = (boundsSize.width - contentsFrame.size.width) / 2.0;
-        } else {
-            contentsFrame.origin.x = 0.0;
-        }
-        
-        if (contentsFrame.size.height < boundsSize.height) {
-            contentsFrame.origin.y = (boundsSize.height - contentsFrame.size.height) / 2.0;
-        } else {
-            contentsFrame.origin.y = 0.0;
-        }
-        
-        self.imageView.frame = contentsFrame;
-    }
-    func viewForZoomingInScrollView(scrollView:UIScrollView) -> UIView{
-        // Return the view that we want to zoom
-        return self.imageView;
-    }
-    
-    func scrollViewDidZoom(scrollView:UIScrollView) {
-    // The scroll view has zoomed, so we need to re-center the contents
-        self.centerScrollViewContents()
-    }
-    
-    func scrollViewDoubleTapped(recognizer:UITapGestureRecognizer) {
-    // Get the location within the image view where we tapped
-       var pointInView:CGPoint = recognizer.locationInView(self.imageView)
-    
-    // Get a zoom scale that's zoomed in slightly, capped at the maximum zoom scale specified by the scroll view
-        var newZoomScale:CGFloat = self.imageScrollView.zoomScale * 1.5;
-        newZoomScale = min(newZoomScale, self.imageScrollView.maximumZoomScale);
-    
-    // Figure out the rect we want to zoom to, then zoom to it
-        var scrollViewSize:CGSize = self.imageScrollView.bounds.size;
-    
-        var w:CGFloat = scrollViewSize.width / newZoomScale;
-        var h:CGFloat = scrollViewSize.height / newZoomScale;
-        var x:CGFloat = pointInView.x - (w / 2.0);
-        var y:CGFloat = pointInView.y - (h / 2.0);
-    
-        var rectToZoomTo:CGRect = CGRectMake(x, y, w, h);
-        self.imageScrollView.zoomToRect(rectToZoomTo, animated: true)
-    }
-    
-    func scrollViewTwoFingerTapped(recognizer:UITapGestureRecognizer) {
-        // Zoom out slightly, capping at the minimum zoom scale specified by the scroll view
-        var newZoomScale:CGFloat = self.imageScrollView.zoomScale / 1.5;
-        newZoomScale = max(newZoomScale, self.imageScrollView.minimumZoomScale);
-        self.imageScrollView.setZoomScale(newZoomScale, animated: true)
-        
-    }
-
+   
 }
 
 class FrameCell: UICollectionViewCell
--- PicFlow++/YRImageZoomingView.swift
+++ PicFlow++/YRImageZoomingView.swift
@@ -7,3 +7,86 @@
 //
 
 import Foundation
+import UIKit
+
+class YRImageZoomingView: UIScrollView,UIScrollViewDelegate {
+    
+    
+    var imageView:UIImageView?
+    var image:UIImage!
+        {
+        didSet{
+           self.imageView?.frame = self.bounds
+           self.imageView?.image = self.image 
+        }
+    }
+   
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        // Initialization code
+        self.setupImageView()
+    }
+    
+    
+    func setupImageView()
+    {
+        self.delegate = self
+        self.imageView = UIImageView(frame:self.bounds)
+        self.imageView!.contentMode = .ScaleAspectFill
+        self.addSubview(self.imageView!)
+        self.showsHorizontalScrollIndicator = false
+        self.showsVerticalScrollIndicator = false
+        self.backgroundColor = UIColor.clearColor()
+        self.minimumZoomScale = 1;
+        self.maximumZoomScale = 3;
+        
+        var doubleTap = UITapGestureRecognizer(target: self, action: "doubleTapped:")
+        doubleTap.numberOfTapsRequired = 2;
+        self.addGestureRecognizer(doubleTap);
+
+    }
+    
+    
+    required init(coder aDecoder: NSCoder) {
+       // fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
+        self.setupImageView()
+    }
+    
+    func doubleTapped(sender:UITapGestureRecognizer)
+    {
+        if self.zoomScale > 1.0
+        {
+            self.setZoomScale(1.0, animated:true);
+        }
+        else
+        {
+            var point = sender.locationInView(self);
+            self.zoomToRect(CGRectMake(point.x-50, point.y-50, 100, 100), animated:true)
+        }
+        
+    }
+    
+    
+    func viewForZoomingInScrollView(scrollView: UIScrollView!)->UIView
+    {
+        return self.imageView!
+    }
+    
+    override func layoutSubviews()
+    {
+        super.layoutSubviews()
+       // self.imageView!.setImage(self.imageURL,placeHolder:placeHolder)
+        self.imageView?.image = self.image
+    }
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect)
+    {
+    // Drawing code
+    }
+    */
+    
+}
